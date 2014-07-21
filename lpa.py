38e62f79ab3ee05d722c5df42141374ac79de595
Brant Young <anatman.xx@gmail.com>
2014-07-21 17:25:17 +0800

    replace graph module with networkx
    remove graph.py
    modify lpa.py replace graph module with networkx
    add f.data (sample data from game similarity project)

--- lpa.py
+++ lpa.py
@@ -1,13 +1,21 @@
-from graph import *
+import networkx as nx
+import matplotlib.pyplot as plt
 
-def lpa_train(graph):
-    res = False
+def read_file(path):
+    graph = nx.read_edgelist(path, data = (('weight', float), ))
+    return graph
 
-    for vid, vertex in graph.vertex.items():
-        for edge in vertex.edge:
+def init(graph):
+    for n, d in graph.nodes_iter():
+        d['lable'] = n
 
-
-    return res
+def lpa(graph):
+    for i in graph.nodes_iter():
+        for n in graph.neighbors_iter(i):
+            pass
 
 if __name__ == '__main__':
-    pass
+    g = read_file('f.data')
+    node_color=[float(g.degree(v)) for v in g]
+    nx.draw(g, node_color = node_color)
+    plt.show()
