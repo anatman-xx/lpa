b83f05f6fbef0731491ccb42648c6868ed5fe523
Brant Young <anatman.xx@gmail.com>
2014-07-29 17:47:14 +0800

    implement part of mpla.

--- mlpa.py
+++ mlpa.py
@@ -11,7 +11,8 @@ def read_graph_from_file(path):
 
     # initial graph node's attribute 'label' with its id
     for node, data in graph.nodes_iter(True):
-        data['label'] = set()
+        data['pre_label'] = {node : 1.0}
+        data['current_label'] = dict()
 
     return graph
 
@@ -26,6 +27,55 @@ def read_game_info_from_file(path):
 
     return game
 
-def mlpa(grash):
-    pass
+# muti-label-propagation algorithm (can find overlapping communities)
+# use synchronous updating for better results
+def mlpa(graph, v):
+    def label(graph, x, c):
+        pass
 
+    for node in graph.nodes_iter():
+        count = {}
+        degree = graph.degree(node)
+
+        for neighbor in graph.neighbors_iter(node):
+            neighbor_label = graph.node[node]['pre_label']
+            neighbor_weight = graph.edge[node][neighbor]['weight']
+
+            count[neighbor_label] = count.setdefault(neighbor_label, 0.0) + 1.0
+
+        print count
+        print degree
+
+def estimate_stop_cond(graph):
+    for node in graph.nodes_iter():
+        count = {}
+
+        for neighbor in graph.neighbors_iter(node):
+            pass
+
+def print_graph_info(graph):
+    game_info = read_game_info_from_file('sample/id_name.info')
+    info = {}
+
+    for node, data in graph.nodes_iter(True):
+        info.setdefault(graph.node[node]['current_label'], []).append(game_info.get(node, node))
+
+    print 'node num:', len(graph.nodes())
+    print 'class num:', len(info.keys())
+    print 'class:', info.keys()
+    print 'info:\n'
+    for clazz in info:
+        print clazz, ':',
+        for label in info[clazz]:
+            print '\'' + label + '\'',
+        print '\n',
+
+if __name__ == '__main__':
+    g = read_graph_from_file('sample/t.data')
+    mlpa(g, 0.5)
+    #print_graph_info(g)
+
+    #node_color = [float(g.node[v]['label']) for v in g]
+    ##labels = dict([(node, node) for node, data in g.nodes_iter(True)])
+    #nx.draw_networkx(g, node_color = node_color)
+    #plt.show()
