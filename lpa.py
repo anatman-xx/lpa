062ecfc73b451e69bb3ae0c5530f81752d214d7f
Brant Young <anatman.xx@gmail.com>
2014-08-02 13:49:44 +0800

    remove some method never used.

--- lpa.py
+++ lpa.py
@@ -64,56 +64,18 @@ def lpa(graph):
                 count[neighbor_label] = count.setdefault(neighbor_label, 0.0) + neighbor_weight
 
             # find out labels with maximum count
-            count_sum = sum(count.values())
-            for key in count:
-                count[key] = count[key] / count_sum
             count_items = count.items()
             count_items.sort(key = lambda x: x[1], reverse = True)
 
             # if there is not only one label with maximum count then choose one randomly
             labels = [(k, v) for k, v in count_items if v == count_items[0][1]]
-            if len(labels) == 0:
-                continue
-
             label = random.sample(labels, 1)[0][0]
 
             graph.node[node]['label'] = label
 
-        if estimate_stop_cond(graph) is True or loop_count > 100:
+        if estimate_stop_cond(graph) is True or loop_count >= 10:
             print 'complete'
             return
-
-def cut_large_community(graph, limit):
-    label_count = {}
-
-    for node in graph.nodes_iter():
-        label_count.setdefault(graph.node[node]['label'], []).append(node)
-
-    large_community = []
-    for nodes in label_count.values():
-        if len(nodes) > limit:
-            sub_graph = nx.Graph(graph.subgraph(nodes))
-            for node in sub_graph.node:
-                sub_graph.node[node]['label'] = node
-            large_community.append(sub_graph)
-
-    return large_community
-
-def crop_edge(graph, value):
-    edges = []
-
-    for u, v, data in graph.edges_iter(data = True):
-        if data['weight'] < value:
-            edges.append((u, v),)
-
-    return edges
-
-def fitness_proportionate_selection(weights):
-    random_value = random.random() * sum(weights)
-    for index, weight in enumerate(weights):
-        random_value -= weight
-        if random_value < 0:
-            return index
 
 def print_graph_info(graph):
     game_info = read_game_info_from_file('sample/id_name.info')
@@ -133,7 +95,7 @@ def print_graph_info(graph):
         print '\n',
 
 if __name__ == '__main__':
-    g = read_graph_from_file('sample/t.data')
+    g = read_graph_from_file('sample/r.data')
     lpa(g)
     print_graph_info(g)
 
@@ -141,5 +103,3 @@ if __name__ == '__main__':
     #labels = dict([(node, node) for node, data in g.nodes_iter(True)])
     nx.draw_networkx(g, node_color = node_color)
     plt.show()
-
-    #sys.exit(0)
